7707d8bda255d3ddbdb5e39143e9b2872530166a
ArifahLailaAdrai <arifahlailaaa12@gmail.com>
2022-04-25 01:05:55 -0700

    uts

---
+++ app/src/main/java/com/arifah/ujian/welcomeactivity.java
@@ -0,0 +1,12 @@
+package com.arifah.ujian;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcomeactivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcomeactivity);
+    }
+}
